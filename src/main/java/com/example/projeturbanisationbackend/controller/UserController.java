9cabf4acf9a9743e841c8c941f22f229d67098db
oumaima bouhmid <omaima.m.bou@gmail.com>
2022-11-28 22:04:40 +0100

    architecture generale

--- src/main/java/com/example/projeturbanisationbackend/controller/UserController.java
+++ src/main/java/com/example/projeturbanisationbackend/controller/UserController.java
@@ -1,4 +1,35 @@
 package com.example.projeturbanisationbackend.controller;
 
+import com.example.projeturbanisationbackend.model.User;
+import com.example.projeturbanisationbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    @Autowired
+    UserService userService;
+    @PostMapping("/save")
+    public User save(@RequestBody User user){
+        return userService.save(user);
+    }
+    @PostMapping("/update")
+    public User update(@RequestBody User user){
+        return userService.update(user);
+    }
+    @GetMapping("/{userId}")
+    public User findById(@PathVariable Long userId){
+        return userService.findById(userId);
+    }
+    @GetMapping("/")
+    public List<User> getAll(){
+        return userService.findAll();
+    }
+    @DeleteMapping("/{userId}")
+    public boolean deleteById(@PathVariable Long userId){
+        return userService.deleteById(userId);
+    }
 }
--- src/main/java/com/example/projeturbanisationbackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/projeturbanisationbackend/service/impl/UserServiceImpl.java
@@ -1,4 +1,48 @@
 package com.example.projeturbanisationbackend.service.impl;
 
-public class UserServiceImpl {
+import com.example.projeturbanisationbackend.model.User;
+import com.example.projeturbanisationbackend.repository.UserRepository;
+import com.example.projeturbanisationbackend.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User update(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findById(Long userId) {
+        return userRepository.findById(userId).get();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public boolean deleteById(Long userId) {
+        try {
+            userRepository.deleteById(userId);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
